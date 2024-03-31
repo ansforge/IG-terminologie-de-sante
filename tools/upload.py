436da84c9112b7541c2c58db9e05f34401c76625
Mael PRIOUR <101335975+M-Priour@users.noreply.github.com>
2024-03-31 18:15:51 +0200

    Create upload.py

---
+++ tools/upload.py
@@ -0,0 +1,54 @@
+import asyncio
+import json
+from fhirpy import AsyncFHIRClient
+
+
+async def main():
+    # Create an instance
+    client = AsyncFHIRClient(
+        'https://smt.esante.gouv.fr/fhir/',
+    )
+
+    # Search for CodeSystem
+    resources = client.resources('CodeSystem')  # Return lazy search set
+    list_codeSystems = await resources.fetch()  
+    for e_codeSystem in list_codeSystems :
+        CodeSystem = await client.reference('CodeSystem', e_codeSystem["id"]).to_resource()
+        try:
+            if( CodeSystem["count"] > 1000) :
+                CodeSystem["concept"] = ""
+                CodeSystem["Content"] = "not-present"
+        except :
+            print("Error : " + e_codeSystem["name"] )
+        with open('./ontoserver/'+ e_codeSystem["name"] + ".json", "w", encoding="utf-8") as f:
+            f.write(json.dumps(CodeSystem))       
+
+    # Search for valueSet
+    resources = client.resources('ValueSet')  # Return lazy search set
+    list_valueSets = await resources.fetch()  
+    for e_valueSet in list_valueSets :
+        ValueSet = await client.reference('ValueSet', e_valueSet["id"]).to_resource()
+        with open('./ontoserver/'+ e_valueSet["name"] + ".json", "w", encoding="utf-8") as f:
+            f.write(json.dumps(ValueSet))       
+
+
+
+ 
+
+ 
+
+
+
+
+
+
+    # Iterate over search set
+    org_resources = client.resources('CodeSystem')
+    # Lazy loading resources page by page with page count = 100
+    #async for org_resource in org_resources.limit(100):
+    #    print(org_resource.serialize())
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
