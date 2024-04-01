4c3551f9686b0e0970ad3ce0b4de549d7b2b750e
Mael PRIOUR <101335975+M-Priour@users.noreply.github.com>
2024-04-01 16:21:44 +0200

    Update upload.py

--- tools/upload.py
+++ tools/upload.py
@@ -13,44 +13,43 @@ async def main():
     resources = client.resources('CodeSystem')  # Return lazy search set
     list_codeSystems = await resources.fetch()  
     for e_codeSystem in list_codeSystems :
+        print (e_codeSystem["name"])
         CodeSystem = await client.reference('CodeSystem', e_codeSystem["id"]).to_resource()
+        f = open('../input/ontoserver/TRE/'+ e_codeSystem["name"] + ".json", "w", encoding="utf-8") 
         try:
             if( CodeSystem["count"] > 1000) :
                 e_codeSystem["Content"] = "not-present"
-                with open('../input/ontoserver/TRE/'+ e_codeSystem["name"] + ".json", "w", encoding="utf-8") as f:
-                    f.write(json.dumps(e_codeSystem))  
+                f.write(json.dumps(e_codeSystem))  
             else :
-                with open('../input/ontoserver/TRE/'+ e_codeSystem["name"] + ".json", "w", encoding="utf-8") as f:
-                    f.write(json.dumps(CodeSystem))                   
+                f.write(json.dumps(CodeSystem))                   
         except :
-            with open('../input/ontoserver/TRE/'+ e_codeSystem["name"] + ".json", "w", encoding="utf-8") as f:
                 f.write(json.dumps(CodeSystem))      
    
+
+ 
 
     # Search for valueSet
     resources = client.resources('ValueSet')  # Return lazy search set
     list_valueSets = await resources.fetch()  
     for e_valueSet in list_valueSets :
+        print (e_valueSet["name"])
         ValueSet = await client.reference('ValueSet', e_valueSet["id"]).to_resource()
         with open('../input/ontoserver/JDV/'+ e_valueSet["name"] + ".json", "w", encoding="utf-8") as f:
             f.write(json.dumps(ValueSet))       
 
-
-
+ 
+    # Search for ConceptMap
+    resources = client.resources('ConceptMap')  # Return lazy search set
+    list_conceptMaps = await resources.fetch()  
+    for e_conceptMaps in list_conceptMaps :
+        print (e_valueSet["ConceptMap"])
+        ConceptMap = await client.reference('ConceptMap', e_valueSet["id"]).to_resource()
+        with open('../input/ontoserver/ASS/'+ e_conceptMaps["name"] + ".json", "w", encoding="utf-8") as f:
+            f.write(json.dumps(ConceptMap))      
  
 
- 
 
 
-
-
-
-
-    # Iterate over search set
-    org_resources = client.resources('CodeSystem')
-    # Lazy loading resources page by page with page count = 100
-    #async for org_resource in org_resources.limit(100):
-    #    print(org_resource.serialize())
 
 
 if __name__ == '__main__':
